8f9f555e1c9ea3cd38c8a5ed0c7800c1b89a9126
ponsfrilus <ponsfrilus@gmail.com>
2017-06-09 17:50:58 +0200

    Now with random quotes

--- main.js
+++ main.js
@@ -11,14 +11,12 @@ app.get('/', function (req, res) {
   res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
   res.header('Expires', '-1')
   res.header('Pragma', 'no-cache')
-  res.send("<h3>myapponk8s</h3><br />" + os.hostname() +
-    "<p>myip.getLocalIP4: " + myip.getLocalIP4() + "</p>" +
-    "<p>myip.getLocalIP6: " + myip.getLocalIP6() + "</p>" +
-    "<p>Oh! And here is your lolcal:" +
-    "<br /><img src=\"http://thecatapi.com/api/images/get?format=src&type=gif\"></p>");
-  //res.render('pages/index');
+  let hostname = os.hostname()
+  res.render('pages/index', {
+    hostname: hostname
+  });
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('k8s test server listening on port 3000!')
 })
