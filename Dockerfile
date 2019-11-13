80a1844527c5077a4f7b49bf3aeb0d3d38e81fb3
Nicolas Borboën <ponsfrilus@gmail.com>
2019-11-13 12:36:22 +0100

    [VER] 1.0.2
    * Remove unused package and update used one to latest version
    * Get rid of "on-build" node image as it's not recommanded to use it 
    anymore
    * Add a cat favicon because I like it
    * Update bootstrap 3 to the latest version
    * Fix the random quote logic
    * Add the Shuttleworth quote

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,11 @@
-FROM node:4-onbuild
+FROM node:lts-slim
+LABEL maintainer="nicolas.borboen@epfl.ch"
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY package.json /usr/src/app/
+RUN npm install && npm cache clean --force
+COPY . /usr/src/app
+
 EXPOSE 3000
--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const express   = require('express')
 const app       = express()
 const os        = require('os')
-const myip      = require('quick-local-ip');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -16,7 +15,15 @@ app.get('/', function (req, res) {
     hostname: hostname
   });
 })
-
+const favicon = new Buffer.from('AAABAAEAEBAAAAEAIABoBAAAFgAAACgAAAAQAAAAIAAAAAEAIAAAAAAAAAQAAMMOAADDDgAAAAAAAAAAAAD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wAAAAD/AAAA/wAAAP8AAAD/AAAA/////wD///8A////AP///wD///8A////AP///wAAAAD/AAAA/////wAAAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/////AAAAAP8AAAD/////AP///wD///8A////AAAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/////AP///wD///8AAAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA//////8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/////AP///wD///8AAAAA/wAAAP///////////wAAAP8AAAD/AAAA////////////AAAA/wAAAP////8A////AP///wD///8A////AAAAAP//////AAAA/wAAAP//////AAAA//////8AAAD/AAAA//////8AAAD/////AP///wD///8A////AAAAAP8AAAD//////wAAAP8AAAD//////wAAAP//////AAAA/wAAAP//////AAAA/wAAAP////8A////AP///wAAAAD/AAAA/wAAAP///////////wAAAP8AAAD/AAAA////////////AAAA/wAAAP8AAAD/////AP///wD///8AAAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/////wD///8A////AAAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP8AAAD/AAAA/wAAAP////8A////AP///wAAAAD/AAAA/wAAAP8AAAD/////AP///wD///8A////AP///wAAAAD/AAAA/wAAAP8AAAD/////AP///wD///8AAAAA/wAAAP////8A////AP///wD///8A////AP///wD///8A////AP///wAAAAD/AAAA/////wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A//8AAPg/AACQEwAAwAcAAAABAADABwAAwAcAAIADAACAAwAAgAMAAIADAACHwwAAn/MAAP//AAD//wAA//8AAA==', 'base64'); 
+app.get("/favicon.ico", function(req, res) {
+ res.statusCode = 200;
+ res.setHeader('Content-Length', favicon.length);
+ res.setHeader('Content-Type', 'image/x-icon');
+ res.setHeader("Cache-Control", "public, max-age=2592000");                // expiers after a month
+ res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());
+ res.end(favicon);
+});
 app.listen(3000, function () {
   console.log('k8s test server listening on port 3000!')
 })
